432608dd513093c8d06a57155cd7ab63b058dc48
Conor Mullen <conor.mullen@gmail.com>
2013-03-28 10:10:11 -0400

    Removing dup files added in by heroku pull

--- modules/model/mongooseConnection.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose')
-, mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/galcon'
-, db = mongoose.connect(mongoUrl);
-
-exports.mongoose = mongoose;
-exports.db = db;
