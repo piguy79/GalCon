c330bef0768fd93f2bf86e76a5401849833ff2e5
Matt Pietal <matthew.pietal@gmail.com>
2013-04-08 17:02:51 -0400

    Set app title to GalCon.  Remove old unused activity.

--- GalCon-Client/GalCon-Client-android/gen/com/xxx/galcon/R.java
+++ GalCon-Client/GalCon-Client-android/gen/com/xxx/galcon/R.java
@@ -15,19 +15,16 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int launchNewGameButton=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_launch_screen=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
         public static final int activity_launch_screen=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int launch_new_game_button=0x7f040003;
         public static final int menu_settings=0x7f040001;
         public static final int title_activity_launch_screen=0x7f040002;
     }
