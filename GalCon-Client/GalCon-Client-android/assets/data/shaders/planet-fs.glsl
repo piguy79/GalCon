0efda3b8287854a4c11d8480f23fcad6735f900a
Matt Pietal <matthew.pietal@gmail.com>
2013-04-13 14:36:02 -0400

    #20 support numbers 0 - 99 on the planet.  still need to clip the '1' a little better

--- GalCon-Client/GalCon-Client-android/assets/data/shaders/planet-fs.glsl
+++ GalCon-Client/GalCon-Client-android/assets/data/shaders/planet-fs.glsl
@@ -16,23 +16,45 @@ uniform sampler2D numbersTex;
 
 varying vec2 vTexCoords;
 
-const float imagehalfWidth = 512.0; 
+const float IMAGE_WIDTH = 512.0; 
 
 float numberOffset(int number);
 
 void main() {
-	float xDistToNumberCenter = 0.5 - vTexCoords.x;
-	float yDistToNumberCenter = 0.5 - vTexCoords.y;
+	float xDistToCenter = 0.5 - vTexCoords.x;
+	float yDistToCenter = 0.5 - vTexCoords.y;
 	float halfWidth = 0.08;
 	float halfHeight = 0.1;
+	
+	float textAreaHalfWidth = 0.08;
+	if(shipCount > 9) {
+		textAreaHalfWidth *= 2.0;
+	}
+	
 	vec4 numberTexColor = vec4(0,0,0,0);
-	if(xDistToNumberCenter < halfWidth && xDistToNumberCenter > -halfWidth 
-		&& yDistToNumberCenter < halfHeight && yDistToNumberCenter > -halfHeight) {
+	if(xDistToCenter < textAreaHalfWidth && xDistToCenter > -textAreaHalfWidth 
+		&& yDistToCenter < halfHeight && yDistToCenter > -halfHeight) {
+		
+		int numberToShow = shipCount;
+		
+		float xNumberCenter = 0.5;
+		if(shipCount > 9) {
+			if(xDistToCenter > 0.0) {
+				xNumberCenter = 0.5 - halfWidth;
+				numberToShow = numberToShow - (numberToShow / 10) * 10;
+			} else {
+				xNumberCenter = 0.5 + halfWidth;
+				numberToShow = numberToShow / 10;
+			}
+		}
+			
+		float xDistToNumberCenter = xNumberCenter - vTexCoords.x;
+		float yDistToNumberCenter = yDistToCenter;
 		xDistToNumberCenter = (xDistToNumberCenter + halfWidth) * (1.0 / (1.0-(halfWidth + halfWidth)));
 		yDistToNumberCenter = (yDistToNumberCenter + halfHeight) * (1.0 / (1.0-(halfHeight + halfHeight)));
 				
 		numberTexColor = texture2D(numbersTex, 
-									vec2(numberOffset(shipCount) + xDistToNumberCenter * 0.18, yDistToNumberCenter * 0.2));
+									vec2(numberOffset(numberToShow) + xDistToNumberCenter * 0.18, yDistToNumberCenter * 0.2));
 	}
 	
 	if(numberTexColor.a == 0.0) {
@@ -45,31 +67,31 @@ void main() {
 
 float numberOffset(int number) {
 	if(number == 0) {
-		return 173.0/imagehalfWidth;
+		return 173.0/IMAGE_WIDTH;
 	}
 	if(number == 1) {
-		return 160.0/imagehalfWidth;
+		return 160.0/IMAGE_WIDTH;
 	}
 	if(number == 2) {
-		return 142.0/imagehalfWidth;
+		return 142.0/IMAGE_WIDTH;
 	}
 	if(number == 3) {
-		return 122.0/imagehalfWidth;
+		return 122.0/IMAGE_WIDTH;
 	}
 	if(number == 4) {
-		return 100.0/imagehalfWidth;
+		return 100.0/IMAGE_WIDTH;
 	}
 	if(number == 5) {
-		return 80.0/imagehalfWidth;
+		return 80.0/IMAGE_WIDTH;
 	}
 	if(number == 6) {
-		return 60.0/imagehalfWidth;
+		return 60.0/IMAGE_WIDTH;
 	}
 	if(number == 7) {
-		return 41.0/imagehalfWidth;
+		return 41.0/IMAGE_WIDTH;
 	}
 	if(number == 8) {
-		return 20.0/imagehalfWidth;
+		return 20.0/IMAGE_WIDTH;
 	}
 	
 	return 0.0;
