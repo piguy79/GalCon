f68fcfc535e6b4e798fd6fe0980dfb9f7a72f457
Matt Pietal <matthew.pietal@gmail.com>
2013-05-02 06:51:09 -0400

    Untrack android files

--- GalCon-Client/GalCon-Client-android/gen/com/xxx/galcon/R.java
+++ GalCon-Client/GalCon-Client-android/gen/com/xxx/galcon/R.java
@@ -15,18 +15,27 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int username=0x7f070001;
+        public static final int usernameErrorMsg=0x7f070000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int create_username=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_launch_screen=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int connectionError=0x7f040008;
+        public static final int create=0x7f040006;
         public static final int menu_settings=0x7f040001;
         public static final int title_activity_launch_screen=0x7f040002;
+        public static final int username=0x7f040003;
+        public static final int usernameTaken=0x7f040007;
+        public static final int welcome_header=0x7f040004;
+        public static final int welcome_text=0x7f040005;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
