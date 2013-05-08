944ae2a659bc7de525cc3328f07aba791d9eef86
Conor Mullen <conor.mullen@gmail.com>
2013-05-08 06:14:21 -0400

    Create a number of ability planets equal to 20% of the "Other" planets. Those which are far away from the home planets

--- GalCon-Server/modules/model/gameType/abilityBasedGameType.js
+++ GalCon-Server/modules/model/gameType/abilityBasedGameType.js
@@ -4,6 +4,9 @@ var SHIP_NUMBER = 10;
 
 exports.addPlanetAbilities = function(planetsFarFromHomes, abilityToAdd){
 
+	var abilitiesToAdd = planetsFarFromHomes.length * 0.2;
+	var addedAbilities = 0;
+
 	for(var i=0; i < planetsFarFromHomes.length; i++){
 		var planetToAddTo = planetsFarFromHomes[i];
 		
@@ -11,7 +14,11 @@ exports.addPlanetAbilities = function(planetsFarFromHomes, abilityToAdd){
 			planetToAddTo.shipRegenRate = SHIP_REGEN_RATE;
 			planetToAddTo.numberOfShips = SHIP_NUMBER;
 			planetToAddTo.ability = abilityToAdd;
-			break;
+			addedAbilities++;
+			
+			if(addedAbilities >= abilitiesToAdd){
+				break;
+			}
 		}
 	}	
 }
