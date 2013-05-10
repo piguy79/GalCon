9b8af65926a8e03ef763676eaeee60cc133765d3
Matt Pietal <matthew.pietal@gmail.com>
2013-05-09 21:24:50 -0400

    Set minimum number of planets that can be available

--- GalCon-Server/routes/index.js
+++ GalCon-Server/routes/index.js
@@ -15,7 +15,8 @@ exports.generateGame = function(req, res) {
 	var playerHandle = req.body.playerHandle;
 
 	userManager.findUserByHandle(playerHandle, function(user) {
-		var numPlanets = req.body.width * req.body.height / 4;
+		var numPlanets = Math.floor(req.body.width * req.body.height / 4);
+		numPlanets = Math.max(12, numPlanets);
 		gameManager.createGame([ user ], req.body.width, req.body.height, numPlanets,
 				req.body.gameType, function(game) {
 					gameManager.saveGame(game, function() {
