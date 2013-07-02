bd2acf6971f647dd33bd4220ec1f66b729995a9b
Conor Mullen <conor.mullen@gmail.com>
2013-07-02 06:23:48 -0400

    Change application name for nodetime

--- GalCon-Server/app.js
+++ GalCon-Server/app.js
@@ -2,7 +2,7 @@
 if(process.env.NODETIME_ACCOUNT_KEY) {
   require('nodetime').profile({
     accountKey: process.env.NODETIME_ACCOUNT_KEY,
-    appName: 'My Application Name' // optional
+    appName: 'Galcon Server'
   });
 }
 
