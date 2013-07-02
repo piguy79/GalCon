756954679dd8b4ec41299758ce1ec93147587a43
Conor Mullen <conor.mullen@gmail.com>
2013-07-02 06:32:56 -0400

    Remove use of nodetime for now

--- GalCon-Server/app.js
+++ GalCon-Server/app.js
@@ -1,10 +1,10 @@
 
-if(process.env.NODETIME_ACCOUNT_KEY) {
-  require('nodetime').profile({
-    accountKey: process.env.NODETIME_ACCOUNT_KEY,
-    appName: 'Galcon Server'
-  });
-}
+//if(process.env.NODETIME_ACCOUNT_KEY) {
+ // require('nodetime').profile({
+  //  accountKey: process.env.NODETIME_ACCOUNT_KEY,
+   // appName: 'Galcon Server'
+ // });
+//}
 
 var express = require('express')
   , routes = require('./routes')
