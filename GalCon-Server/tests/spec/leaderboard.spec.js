9c4d9f81e4139ef9d338f2336083e01c88fb8fbe
Matt Pietal <matthew.pietal@gmail.com>
2013-08-21 13:37:16 -0400

    Add tests for two players posting to a leaderboard

--- GalCon-Server/tests/spec/leaderboard.spec.js
+++ GalCon-Server/tests/spec/leaderboard.spec.js
@@ -3,23 +3,22 @@ var leaderboard = require('../../modules/model/leaderboard'), Step = require('st
 describe("Leaderboard tests", function() {
 	var boardId1 = "TEST_BOARD1";
 	var boardId2 = "TEST_BOARD2";
-	var playerHandle = "TEST_HANDLE";
+	var playerHandle1 = "TEST_HANDLE1";
+	var playerHandle2 = "TEST_HANDLE2";
 
-	afterEach(function(done) {
-		leaderboard.LeaderboardModel.remove({
-			"playerHandle" : playerHandle
-		}, function(err) {
+	beforeEach(function(done) {
+		leaderboard.LeaderboardModel.remove().where("playerHandle").in([playerHandle1, playerHandle2]).exec(function(err) {
 			done();
 		});
 	});
 
 	it("Add one new leaderboard row", function(done) {
 		Step(function updateScore() {
-			leaderboard.updateScore(boardId1, playerHandle, 1, this);
+			leaderboard.updateScore(boardId1, playerHandle1, 1, this);
 		}, function retrieveScore(err, leaderboardRow) {
 			leaderboard.LeaderboardModel.findOne({
 				"boardId" : boardId1,
-				"playerHandle" : playerHandle
+				"playerHandle" : playerHandle1
 			}, this);
 		}, function validateScore(err, leaderboardRow) {
 			expect(err).toBe(null);
@@ -28,15 +27,15 @@ describe("Leaderboard tests", function() {
 		});
 	});
 
-	it("Add two leaderboard rows for the same user/board", function(done) {
+	it("Add one leaderboard row, then increment the score for one user", function(done) {
 		Step(function updateScore() {
-			leaderboard.updateScore(boardId1, playerHandle, 4, this);
+			leaderboard.updateScore(boardId1, playerHandle1, 4, this);
 		}, function updateScore(err, leaderboardRow) {
-			leaderboard.updateScore(boardId1, playerHandle, 6, this);
+			leaderboard.updateScore(boardId1, playerHandle1, 6, this);
 		}, function retrieveScore(err, leaderboardRow) {
 			leaderboard.LeaderboardModel.findOne({
 				"boardId" : boardId1,
-				"playerHandle" : playerHandle
+				"playerHandle" : playerHandle1
 			}, this);
 		}, function validateScore(err, leaderboardRow) {
 			expect(err).toBe(null);
@@ -47,13 +46,13 @@ describe("Leaderboard tests", function() {
 
 	it("Add two leaderboard rows for the same user, different boards", function(done) {
 		Step(function updateScore() {
-			leaderboard.updateScore(boardId1, playerHandle, 4, this);
+			leaderboard.updateScore(boardId1, playerHandle1, 4, this);
 		}, function updateScore(err, leaderboardRow) {
-			leaderboard.updateScore(boardId2, playerHandle, 5, this);
+			leaderboard.updateScore(boardId2, playerHandle1, 5, this);
 		}, function retrieveScore(err, leaderboardRow) {
 			leaderboard.LeaderboardModel.findOne({
 				"boardId" : boardId1,
-				"playerHandle" : playerHandle
+				"playerHandle" : playerHandle1
 			}, this);
 		}, function validateScore(err, leaderboardRow) {
 			expect(err).toBe(null);
@@ -62,7 +61,33 @@ describe("Leaderboard tests", function() {
 		}, function retrieveScore(err) {
 			leaderboard.LeaderboardModel.findOne({
 				"boardId" : boardId2,
-				"playerHandle" : playerHandle
+				"playerHandle" : playerHandle1
+			}, this);
+		}, function validateScore(err, leaderboardRow) {
+			expect(err).toBe(null);
+			expect(leaderboardRow.score).toBe(5);
+			done();
+		});
+	});
+	
+	it("Add two leaderboard rows for different users, same board", function(done) {
+		Step(function updateScore() {
+			leaderboard.updateScore(boardId1, playerHandle1, 4, this);
+		}, function updateScore(err, leaderboardRow) {
+			leaderboard.updateScore(boardId1, playerHandle2, 5, this);
+		}, function retrieveScore(err, leaderboardRow) {
+			leaderboard.LeaderboardModel.findOne({
+				"boardId" : boardId1,
+				"playerHandle" : playerHandle1
+			}, this);
+		}, function validateScore(err, leaderboardRow) {
+			expect(err).toBe(null);
+			expect(leaderboardRow.score).toBe(4);
+			return null;
+		}, function retrieveScore(err) {
+			leaderboard.LeaderboardModel.findOne({
+				"boardId" : boardId1,
+				"playerHandle" : playerHandle2
 			}, this);
 		}, function validateScore(err, leaderboardRow) {
 			expect(err).toBe(null);
