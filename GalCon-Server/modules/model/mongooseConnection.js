d68f8a7fd5377e96168bb9991d5f4482e87261f2
Matt Pietal <matthew.pietal@gmail.com>
2013-10-18 06:29:22 -0400

    Make withPromise method sharable

--- GalCon-Server/modules/model/mongooseConnection.js
+++ GalCon-Server/modules/model/mongooseConnection.js
@@ -2,23 +2,8 @@ var mongoose = require('mongoose')
 , mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/galcon'
 , db = mongoose.connect(mongoUrl);
 
-mongoose.Model.withPromise = function(func){
-	var prom = new mongoose.Promise();
-	var array = Array.prototype.slice.call(arguments, 1);
-	array.push(function(err, result){
-		if(err){
-			prom.reject(err);
-		}else{
-			prom.complete(result);
-		}
-	});
-	func.apply(this, array);
-	return prom;
-}
-
-mongoose.Document.prototype.withPromise = function(func) {
+var withPromise = function(func) {
 	var p = new mongoose.Promise();
-	p.complete();
 	var array = Array.prototype.slice.call(arguments, 1);
 	array.push(function(err, result) {
 		if(err) {
@@ -30,6 +15,9 @@ mongoose.Document.prototype.withPromise = function(func) {
 	func.apply(this, array);
 	return p;
 }
+
+mongoose.Model.withPromise = withPromise;
+mongoose.Document.prototype.withPromise = withPromise;
 
 exports.mongoose = mongoose;
 exports.db = db;
