e5cb607ce5fcd4d2011a8bb74cfa6d2f07fd098f
Conor Mullen <conor.mullen@gmail.com>
2014-01-31 06:44:27 -0500

    Format the population count

--- GalCon-Client/GalCon-Client/src/com/xxx/galcon/screen/ship/selection/PlanetInformationDialog.java
+++ GalCon-Client/GalCon-Client/src/com/xxx/galcon/screen/ship/selection/PlanetInformationDialog.java
@@ -1,5 +1,7 @@
 package com.xxx.galcon.screen.ship.selection;
 
+import java.text.NumberFormat;
+
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -65,7 +67,7 @@ public class PlanetInformationDialog extends CancelEnabledDialog {
 		ShaderLabel regenName = new ShaderLabel(fontShader, "Regen Rate: ", skin, Constants.UI.DEFAULT_FONT_BLACK);
 		ShaderLabel regenRate = new ShaderLabel(fontShader, Math.round(planet.shipRegenRate) + "", skin, Constants.UI.DEFAULT_FONT);
 		ShaderLabel populationName = new ShaderLabel(fontShader, "Population: ", skin, Constants.UI.DEFAULT_FONT_BLACK);
-		ShaderLabel populationValue = new ShaderLabel(fontShader, "" + Math.round(planet.population), skin, Constants.UI.DEFAULT_FONT);
+		ShaderLabel populationValue = new ShaderLabel(fontShader, calculatePopulation(), skin, Constants.UI.DEFAULT_FONT);
 		ShaderLabel abilityName = new ShaderLabel(fontShader, "Ability: ", skin, Constants.UI.DEFAULT_FONT_BLACK);
 		ShaderLabel abilityValue = new ShaderLabel(fontShader, AbilityDisplay.abilityDisplayNames.get(planet.ability), skin, Constants.UI.DEFAULT_FONT);
 		ShaderLabel harvestName = new ShaderLabel(fontShader, "Rounds until Death:", skin, Constants.UI.DEFAULT_FONT_BLACK);
@@ -91,6 +93,12 @@ public class PlanetInformationDialog extends CancelEnabledDialog {
 
 		}
 		
+	}
+
+	private String calculatePopulation() {
+		NumberFormat format = NumberFormat.getInstance();
+		
+		return "" + format.format(Math.round(planet.population));
 	}
 
 	private int findRoundsRemainingInHarvest() {
