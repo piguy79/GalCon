39d62c025135d51997b1f7bdaf917897cfcb253a
Matt Pietal <matthew.pietal@gmail.com>
2014-02-20 10:10:04 -0500

    Adding new iOS game action method

--- GalCon-Client/GalCon-Client-robovm/src/com/xxx/galcon/IOSGameAction.java
+++ GalCon-Client/GalCon-Client-robovm/src/com/xxx/galcon/IOSGameAction.java
@@ -25,7 +25,7 @@ public class IOSGameAction implements GameAction {
 	@Override
 	public void setGameLoop(GameLoop gameLoop) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -37,165 +37,165 @@ public class IOSGameAction implements GameAction {
 	@Override
 	public void setSession(String session) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void exchangeTokenForSession(UIConnectionResultCallback<Session> callback, String authProvider, String token) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void matchPlayerToGame(UIConnectionResultCallback<GameBoard> callback, String handle, Long mapToFind) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findAvailableGames(UIConnectionResultCallback<AvailableGames> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findAllMaps(UIConnectionResultCallback<Maps> callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findGameById(UIConnectionResultCallback<GameBoard> callback, String gameId, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void joinGame(UIConnectionResultCallback<GameBoard> callback, String gameId, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resignGame(UIConnectionResultCallback<GameBoard> callback, String gameId, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void performMoves(UIConnectionResultCallback<GameBoard> callback, String gameId, List<Move> moves,
 			List<HarvestMove> harvestMoves) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findCurrentGamesByPlayerHandle(UIConnectionResultCallback<AvailableGames> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findUserInformation(UIConnectionResultCallback<Player> callback, String email) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void searchForPlayers(UIConnectionResultCallback<People> callback, String searchTerm) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findConfigByType(UIConnectionResultCallback<Configuration> callback, String type) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void requestHandleForEmail(UIConnectionResultCallback<HandleResponse> callback, String email, String handle) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void findGamesWithPendingMove(UIConnectionResultCallback<AvailableGames> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addFreeCoins(UIConnectionResultCallback<Player> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addCoinsForAnOrder(UIConnectionResultCallback<Player> callback, String handle, List<Order> orders) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteConsumedOrders(UIConnectionResultCallback<Player> callback, String handle, List<Order> orders) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void reduceTimeUntilNextGame(UIConnectionResultCallback<Player> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void showAd(AdColonyVideoListener listener) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void loadAvailableInventory(UIConnectionResultCallback<Inventory> callback) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void recoverUsedCoinCount(UIConnectionResultCallback<Player> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void invitePlayerForGame(UIConnectionResultCallback<GameBoard> callback, String requesterHandle,
 			String inviteeHandle, Long mapKey) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findFriends(UIConnectionResultCallback<People> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void findPendingIvites(UIConnectionResultCallback<GameQueue> callback, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void acceptInvite(UIConnectionResultCallback<GameBoard> callback, String gameId, String handle) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void declineInvite(UIConnectionResultCallback<BaseResult> callback, String gameId, String handle) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void requestHandleForId(UIConnectionResultCallback<HandleResponse> callback, String id, String handle) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
