b0ba3357e23b5eb294a8e7fa569455768d09f35b
Matt Pietal <matthew.pietal@gmail.com>
2014-04-21 16:56:48 -0400

    Properly clean up test data

--- GalCon-Server/tests/spec/findAvailableGames.spec.js
+++ GalCon-Server/tests/spec/findAvailableGames.spec.js
@@ -36,7 +36,7 @@ describe("Find available games -", function() {
 			if(err) { console.log(err); }
 			mapManager.MapModel.remove().where("key").in([MAP_KEY_1, MAP_KEY_2]).exec(function(err) {
 				if(err) { console.log(err); }
-				userManager.UserModel.remove().where("handle").in([PLAYER_1_HANDLE, PLAYER_2_HANDLE]).exec(function(err) {
+				userManager.UserModel.remove().where("handle").in([PLAYER_1_HANDLE, PLAYER_2_HANDLE, PLAYER_3_HANDLE]).exec(function(err) {
 					if(err) { console.log(err); }
 					done();
 				});
