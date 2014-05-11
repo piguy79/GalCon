e5d630547d8183b9402866f7f6f61925bf363ca4
Conor Mullen <conor.mullen@gmail.com>
2014-05-11 09:39:27 -0400

    Handle cancel on FB login

--- GalCon-Client/GalCon-Client-robovm/src/com/railwaygames/solarsmash/social/FacebookAuthorization.java
+++ GalCon-Client/GalCon-Client-robovm/src/com/railwaygames/solarsmash/social/FacebookAuthorization.java
@@ -63,6 +63,7 @@ public class FacebookAuthorization extends UIApplicationDelegateAdapter implemen
 				@Override
 				public void onCancel() {
 					Gdx.app.log("FB" ,"Login cancelled.");
+					listener.onSignInFailed("");
 				}
 			});
 		}else{
