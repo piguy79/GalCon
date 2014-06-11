cba2a44a8728c641a487e3426b4d2c78e29bc873
Matt Pietal <matthew.pietal@gmail.com>
2014-06-11 16:52:15 -0400

    Leaderboards: integrate g+ friends

--- GalCon-Client/GalCon-Client/src/com/railwaygames/solarsmash/model/Leaderboards.java
+++ GalCon-Client/GalCon-Client/src/com/railwaygames/solarsmash/model/Leaderboards.java
@@ -1,6 +1,7 @@
 package com.railwaygames.solarsmash.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -11,33 +12,39 @@ import com.railwaygames.solarsmash.model.base.JsonConvertible;
 
 public class Leaderboards extends JsonConvertible {
 
-	public List<Leaderboard> leaderboards = new ArrayList<Leaderboard>();
+	public java.util.Map<String, List<LeaderboardEntry>> leaderboards = new HashMap<String, List<LeaderboardEntry>>();
 
 	@Override
 	protected void doConsume(JSONObject jsonObject) throws JSONException {
 		JSONArray boards = jsonObject.optJSONArray("boards");
 		if (boards != null && boards.length() > 0) {
 			for (int i = 0; i < boards.length(); ++i) {
-				JSONObject board = boards.getJSONObject(i);
+				JSONArray entriesForBoard = boards.getJSONArray(i);
+				if (entriesForBoard != null && entriesForBoard.length() > 0) {
+					List<LeaderboardEntry> entries = new ArrayList<LeaderboardEntry>();
+					for (int j = 0; j < entriesForBoard.length(); ++j) {
+						JSONObject entry = entriesForBoard.getJSONObject(j);
+						LeaderboardEntry lb = new LeaderboardEntry();
+						lb.id = entry.getString("id");
+						lb.score = (float) entry.getDouble("score");
 
-				Leaderboard lb = new Leaderboard();
-				lb.id = board.getString("id");
-				lb.score = (float) board.getDouble("score");
-				
-				JSONObject user = board.getJSONObject("user");
-				lb.handle = user.getString("handle");
-				lb.userId = user.getString("id");
-				
-				JSONObject record = board.getJSONObject("record");
-				lb.wins = record.getInt("w");
-				lb.losses = record.getInt("l");
+						JSONObject user = entry.getJSONObject("user");
+						lb.handle = user.getString("handle");
+						lb.userId = user.getString("id");
 
-				leaderboards.add(lb);
+						JSONObject record = entry.getJSONObject("record");
+						lb.wins = record.getInt("w");
+						lb.losses = record.getInt("l");
+
+						entries.add(lb);
+					}
+					leaderboards.put(entries.get(0).id, entries);
+				}
 			}
 		}
 	}
 
-	public static class Leaderboard {
+	public static class LeaderboardEntry {
 		public float score;
 		public String handle;
 		public String userId;
